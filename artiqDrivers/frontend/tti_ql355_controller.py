d3e9ff06f8073141a9da5679411d3cd25b87fbf9
ALWSDAQ40 <ballance@physics.ox.ac.uk>
2016-06-03 15:30:12 +0100

    Wrote, but did not test, TTI QL355P / QL355TP driver

---
+++ artiqDrivers/frontend/tti_ql355_controller.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3.5
+
+import argparse
+import sys
+
+from artiqDrivers.devices.tti_ql355.driver import QL355
+from artiq.protocols.pc_rpc import simple_server_loop
+from artiq.tools import verbosity_args, simple_network_args, init_logger
+
+def get_argparser():
+    parser = argparse.ArgumentParser(description="ARTIQ controller for TTI QL355P (TP) single (triple) channel power supplies")
+    simple_network_args(parser, 4002)
+    parser.add_argument("-d", "--device", default=None,
+                        help="serial device. See documentation for how to "
+                             "specify a USB Serial Number.")
+    verbosity_args(parser)
+    return parser
+
+
+def main():
+    args = get_argparser().parse_args()
+    init_logger(args)
+
+    if args.device is None:
+        print("You need to specify a -d/--device "
+              "argument. Use --help for more information.")
+        sys.exit(1)
+
+    dev = QL355(args.device)
+
+    # Q: Why not use try/finally for port closure?
+    # A: We don't want to try to close the serial if sys.exit() is called,
+    #    and sys.exit() isn't caught by Exception
+    try:
+        simple_server_loop({"ql355": dev}, args.bind, args.port)
+    except Exception:
+        dev.close()
+    else:
+        dev.close()
+        
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ scripts = [
     "arduinoDds_controller=artiqDrivers.frontend.arduinoDds_controller:main",
     "trapDac_controller=artiqDrivers.frontend.trapDac_controller:main",
     "thorlabs_mdt69xb_controller=artiqDrivers.frontend.thorlabs_mdt69xb_controller:main",
-    "rohdeSynth_controller=artiqDrivers.frontend.rohdeSynth_controller:main"
+    "rohdeSynth_controller=artiqDrivers.frontend.rohdeSynth_controller:main",
+    "tti_ql355_controller=artiqDrivers.frontend.tti_ql355_controller:main"
 ]
 
 setup(name='artiqDrivers',
@@ -20,6 +21,7 @@ setup(name='artiqDrivers',
               'artiqDrivers.devices.rohdeSynth',
               'artiqDrivers.devices.thorlabs_mdt69xb',
               'artiqDrivers.devices.trapDac',
+              'artiqDrivers.devices.tti_ql355'
              ],
     entry_points={
         "console_scripts": scripts,

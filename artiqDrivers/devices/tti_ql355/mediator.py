12bc322e0b45f6702778e62edf3abd022392f434
CJB home <ballance@physics.ox.ac.uk>
2016-06-04 13:19:17 +0100

    Removed stray argument from mediator init

--- artiqDrivers/devices/tti_ql355/mediator.py
+++ artiqDrivers/devices/tti_ql355/mediator.py
@@ -10,7 +10,7 @@ class PsuWrapper:
         'mappings', a dictionary mapping logical devices names to
             (device,channel) tuples
     """
-    def __init__(self, dmgr, devices, mappings, slow_scan):
+    def __init__(self, dmgr, devices, mappings):
         self.core = dmgr.get("core")
         self.devices = { dev: dmgr.get(dev) for dev in devices }
         

38eede93c5496e224e23d5f20d003fca4b0ebaa1
oldlabMaster <none>
2016-06-07 13:32:15 +0100

    fix typos, add debugging comments

--- artiqDrivers/devices/coherentDds/ramanDdsWrapper.py
+++ artiqDrivers/devices/coherentDds/ramanDdsWrapper.py
@@ -26,26 +26,28 @@ class RamanDdsWrapper:
     def setProfile(self, channel, profile, freq, phase=0.0, amp=1.0, addQubitFreq = True):
         ''' channnel: rPara or rV, profile: 0...7, if addQubitFreq=True: the lasers used to create the frequency difference are split by 3.2GHz '''
         if addQubitFreq:
-            freqDDS = fself.msDiff-self.rH_freq-freq
+            freqDDS = self.msDiff-self.rH_freq-freq
         else:
             freqDDS = freq - self.rH_freq
         
-        if channel == rPara:
+        if channel == 'rPara':
             # rPara is double passed +1,+1
             freqDDS /= 2
             phase /= 2
             if (freqDDS<self.rParaRange[0]) or (freqDDS>self.rParaRange[1]):
                 raise ValueError("Rpara frequency out of range, {:.0f}MHz not in [{:.0f},{:.0f}]MHz".format(freqDDS/1e6,self.rParaRange[0]/1e6,self.rParaRange[1]/1e6))
             else:
-                self.dds.setProfile(channel, profile, freqDDS, phase=phase, amp=amp)
+                print('Profile:',profile)
+                print(type(profile))
+                self.dds.setProfile(0, profile, freqDDS, phase=phase, amp=amp)
             
-        elif channel == rV:
+        elif channel == 'rV':
             # rV is -1st order  
             freqDDS *= -1     
             if (freqDDS<self.rVRange[0]) or (freqDDS>self.rVRange[1]):
                 raise ValueError("Rpara frequency out of range, {:.0f}MHz not in [{:.0f},{:.0f}]MHz".format(freqDDS/1e6,self.rVRange[0]/1e6,self.rVRange[1]/1e6))
             else:
-                self.dds.setProfile(channel, profile, freqDDS, phase=phase, amp=amp)
+                self.dds.setProfile(1, profile, freqDDS, phase=phase, amp=amp)
             
         else:
             raise ValueError("Channel can only be rPara or rV")

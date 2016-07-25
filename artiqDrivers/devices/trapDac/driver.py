4b14d3d1a2ef502d5c8fb2ca0bfeff27427129a6
Chris Ballance <ballance@physics.ox.ac.uk>
2016-07-25 17:20:22 +0100

    trapDac/driver : sort out whitespace

--- artiqDrivers/devices/trapDac/driver.py
+++ artiqDrivers/devices/trapDac/driver.py
@@ -9,18 +9,21 @@ logger = logging.getLogger(__name__)
 
 
 class TrapDac:
-	def __init__(self, addr_dc_iface=None, addr_rf_iface=None):
+    def __init__(self, addr_dc_iface=None, addr_rf_iface=None):
         self.dc_iface = OldlabDCInterface(addr_dc_iface)
         self.rf_iface = OldlabRFAttenuatorInterface(addr_rf_iface)
 
     def set_rf_level(self, rf_level):
-    	"""Set the RF level in dB.
-    	Valid range -31.5 to 0"""
-    	self.rf_iface.set_atten(-rf_level)
+        """Set the RF level in dB.
+        Valid range -31.5 to 0"""
+        self.rf_iface.set_atten(-rf_level)
 
     def set_dc(self, dc_vec):
-    	"""Set all 5 dc channels simultaneously"""
-    	self.dc_iface.set_all_dac_channels(*dec_vec)
+        """Set all 5 dc channels simultaneously"""
+        self.dc_iface.set_all_dac_channels(*dec_vec)
+
+    def ping(self):
+        return True
 
 
 class OldlabDCInterface:
@@ -29,14 +32,14 @@ class OldlabDCInterface:
         time.sleep(2)
     
     def set_dac_channel(self, channel=0, value=0): 
-    	"""Sets a given DAC channel to a given value in Volts"""
+        """Sets a given DAC channel to a given value in Volts"""
         assert(channel>=0)
         assert(channel<5)
 
         self.ser.write('v {} {:3.3f}\n'.format(channel,value).encode())
 
     def set_all_dac_channels(self, ch0, ch1, ch2, ch3, ch4): 
-    	"""Simultaneously set all DAC channels"""
+        """Simultaneously set all DAC channels"""
         self.ser.write('va {:3.3f} {:3.3f} {:3.3f} {:3.3f} {:3.3f}\n'.format(ch0, ch1, ch2, ch3, ch4).encode())
 
 
@@ -46,11 +49,9 @@ class OldlabRFAttenuatorInterface:
         time.sleep(2)
     
     def set_atten(self, value=0):
-    	"""Set the attenutation in dB"""
+        """Set the attenutation in dB"""
         atten = math.floor( value*2 + 0.5)/2.0
         atten = min(atten,31.5)
         atten = max(atten,0)
         attenLSB = int(atten/0.5) # Attenutation value in hardware units of 0.5dB
         self.ser.write('atten {}\n'.format(attenLSB).encode())
-    
-    
